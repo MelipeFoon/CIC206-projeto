46a839252e06eebc2dc7abbd821ed5c575402f23
MelipeFoon <felipe.t.moon@gmail.com>
2024-10-03 18:47:31 -0300

    sfaokw

--- maquina_de_vendas.py
+++ maquina_de_vendas.py
@@ -1,13 +1,12 @@
 from tkinter import *
 from tkinter import ttk
 
-I = ['0.25', '0.50', '1.00']
-# I = ['b', '0.25', '0.50', '1.00']
+# Descrição formal do AFD
+I = [0.25, 0.50, 1.00]
 S_ = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 s0 = 0
 F = [8]
 delta = [[1, 2, 4], [2, 3, 5], [3, 4, 6], [4, 5, 7], [5, 6, 8], [6, 7, 8], [7, 8, 8], [8, 8, 8], [8, 8, 8]]
-# delta = [[0, 1, 2, 4], [1, 2, 3, 5], [2, 3, 4, 6], [3, 4, 5, 7], [4, 5, 6, 8], [5, 6, 7, 8], [6, 7, 8, 8], [7, 8, 8, 8], [0, 8, 8, 8]]
 
 def f(estado, entrada):
     return delta[estado][I.index(entrada)]
@@ -18,8 +17,7 @@ def g(estado):
     else:
         return False
 
-def botao(*args):
-    cadeia = entr.get().strip().split(" ")
+def botao():
     saldo = 0
     estado = s0
     fim_cadeia = False
@@ -28,9 +26,11 @@ def botao(*args):
         while not fim_cadeia:
             if i == len(cadeia):
                 fim_cadeia = True
+                cadeia.clear()
+                sld.set(0.0)
             else:
                 estado = f(estado, cadeia[i])
-                saldo += float(cadeia[i])
+                saldo += cadeia[i]
                 # print(estado)
             i+=1
         if g(estado) and saldo > 2:
@@ -51,38 +51,53 @@ def botao(*args):
         resposta.set("Erro executando o autômato: ", e)
         # print("Erro executando o autômato: ", e)
 
+cadeia = []
 
+# Moeda de 25 centavos
+def add025():
+    cadeia.append(0.25)
+    sld.set(sum(cadeia))
+
+# Moeda de 50 centavos
+def add050():
+    cadeia.append(0.50)
+    sld.set(sum(cadeia))
+
+# Moeda de 1 real
+def add100():
+    cadeia.append(1.00)
+    sld.set(sum(cadeia))
+
+# Criação da GUI
 root = Tk()
 root.title("Máquina de vendas")
-
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-entr = StringVar()
-entrada_cadeia = ttk.Entry(mainframe, width=7, textvariable=entr)
-entrada_cadeia.grid(column=2, row=1, sticky=(W, E))
+# Rótulos
+# Indicador do saldo acumulado
+sld = StringVar()
+sld.set(0.0)
+ttk.Label(mainframe, textvariable=sld).grid(column=3, row=2, sticky=(W, E))
 
+# Indicador da resposta
 resposta = StringVar()
-ttk.Label(mainframe, textvariable=resposta).grid(column=2, row=2, sticky=(W, E))
+ttk.Label(mainframe, textvariable=resposta).grid(column=2, row=5, sticky=(W, E))
 
-ttk.Button(mainframe, text="Dispensar", command=botao).grid(column=3, row=3, sticky=W)
+ttk.Label(mainframe, text="Saldo").grid(column=4, row=2, sticky=W)
+ttk.Label(mainframe, text="Resposta:").grid(column=1, row=5, sticky=W)
 
-ttk.Label(mainframe, text="saldo").grid(column=3, row=1, sticky=W)
-# ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="resposta").grid(column=3, row=2, sticky=W)
+# Botões com valores das moedas
+ttk.Button(mainframe, text="0.25", command=add025).grid(column=1, row=1, sticky=N)
+ttk.Button(mainframe, text="0.50", command=add050).grid(column=2, row=1, sticky=N)
+ttk.Button(mainframe, text="1.00", command=add100).grid(column=3, row=1, sticky=N)
+
+# Botão de dispensar o refrigerante
+ttk.Button(mainframe, text="Dispensar", command=botao).grid(column=4, row=1, sticky=W)
 
 for child in mainframe.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
-
-entrada_cadeia.focus()
 root.bind("<Return>", botao)
-
 root.mainloop()
-
-# testes
-# if __name__ == '__main__':
-#     botao(['1.00', '1.00']) # aceita
-#     botao(['0.25', '1.00']) #  rejeita
-#     botao(['1.00', '1.00', '1.00', '0.50'])
