f2e9bcb115d515134c0565ba4bdfbdccaa338745
Felipe Moon <felipe.t.moon@gmail.com>
2024-10-03 20:12:36 -0300

    jfa9n

--- maquina_de_vendas.py
+++ maquina_de_vendas.py
@@ -3,19 +3,24 @@ from tkinter import ttk
 
 # Descrição formal do AFD
 I = [0.25, 0.50, 1.00]
+O = ['r', 'n', 't']
 S_ = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 s0 = 0
 F = [8]
-delta = [[1, 2, 4], [2, 3, 5], [3, 4, 6], [4, 5, 7], [5, 6, 8], [6, 7, 8], [7, 8, 8], [8, 8, 8], [8, 8, 8]]
+delta = [[1, 2, 4], [2, 3, 5], [3, 4, 6], [4, 5, 7], [5, 6, 8], [6, 7, 8], [7, 8, 8], [8, 8, 8], [8, 8, 8]] # Tabela de estado próximo estado
+delta2 = [['n', 'n', 'n'], ['n', 'n', 'n'], ['n', 'n', 'n'], ['n', 'n', 'n'], ['n', 'n', 'n'], ['n', 'n', 't'], ['n', 'n', 't'], ['n', 't', 't'], ['t', 't', 't']] # Saída
 
-def f(estado, entrada):
-    return delta[estado][I.index(entrada)]
+# função de transição
+def f(estd, entrada):
+    return delta[estd][I.index(entrada)]
 
-def g(estado):
-    if estado in F:
-        return True
-    else:
-        return False
+# função de saída
+def g(estd, cad):
+    if len(cad) <= 0:
+        return 'n'
+    if sum(cad) == 2:
+        return 'r'
+    return delta2[estd][I.index(cad[-1])]
 
 def botao():
     saldo = 0
@@ -26,30 +31,33 @@ def botao():
         while not fim_cadeia:
             if i == len(cadeia):
                 fim_cadeia = True
-                cadeia.clear()
                 sld.set(0.0)
             else:
                 estado = f(estado, cadeia[i])
                 saldo += cadeia[i]
-                # print(estado)
-            i+=1
-        if g(estado) and saldo > 2:
-            resposta.set(f"Aqui está seu refrigerante!\nTroco: R$ {saldo-2:.2f}")
-            # print(f"Aqui está seu refrigerante!\nTroco: R$ {saldo-2:.2f}")
 
-        elif saldo == 2:
+            i+=1
+
+        if g(estado, cadeia) == O[2]:
+            resposta.set(f"Aqui está seu refrigerante!\nTroco: R$ {saldo-2:.2f}")
+            cadeia.clear()
+
+        elif g(estado, cadeia) == O[0]:
             resposta.set(f"Aqui está seu refrigerante!")
-            # print(f"Aqui está seu refrigerante!")
+            cadeia.clear()
 
         else:
             resposta.set(f"Saldo insuficiente")
-            # print(f"Saldo insuficiente")
+            cadeia.clear()
+
     except ValueError:
         resposta.set(f"A cadeia foi rejeitada")
-        # print(f"A cadeia foi rejeitada")
+        cadeia.clear()
+ 
     except Exception as e:
         resposta.set("Erro executando o autômato: ", e)
-        # print("Erro executando o autômato: ", e)
+        cadeia.clear()
+
 
 cadeia = []
 
